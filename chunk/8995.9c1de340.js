50e3f8fdeb0b690a7a08a23b75565b83cb353e31
synthetix-team <engineering@snxdao.io>
2024-10-03 06:23:17 +0000

    Release
    Source: https://github.com/Synthetixio/liquidity-ui
    
    Commit: dfaef26093d12b3a2d1296c818af43b625cb0122

--- chunk/8995.9c1de340.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunk_snx_v3_liquidity=globalThis.webpackChunk_snx_v3_liquidity||[]).push([[8995],{58995:s=>{s.exports=JSON.parse('[{"synthMarketId":"1","address":"0x08b2A7e830258F28c9c04501447a8bc83DCE42bE","symbol":"snxETH","name":"Synthetic Ethereum","decimals":18}]')}}]);
